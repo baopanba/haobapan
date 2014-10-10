1eb991e8700a1378869887922867bcb5a6c39e3e
baopanba <hailongt999@126.com>
2014-10-10 20:55:26 +0800

    Update logo.js

--- xiababa/logo.js
+++ xiababa/logo.js
@@ -1,1 +1,1 @@
-document.writeln("<img src=\"http://ww4.sinaimg.cn/large/67b8dccatw1el6cjrwvu5j204r01oa9z.jpg\" width=\"179\" height=\"65\"/>");
+document.writeln("<img src=\"http://ww1.sinaimg.cn/large/67b8dccatw1el6cvx3m21j204r01oa9z.jpg\" width=\"179\" height=\"65\"/>");
