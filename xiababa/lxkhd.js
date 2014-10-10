429526bde8794f784945ef7cca8f7493ddca5c32
baopanba <hailongt999@126.com>
2014-10-10 20:51:30 +0800

    Update lxkhd.js

--- xiababa/lxkhd.js
+++ xiababa/lxkhd.js
@@ -1,1 +1,1 @@
-document.writeln("<a href=\"http://leshiba.github.io/xiazai/\" ><img src=\"http://ww4.sinaimg.cn/large/67b8dccatw1el6cp87cq1j205f035mx9.jpg\" ></a>");
+document.writeln("<a href=\"http://leshiba.github.io/xiazai/\" ><img src=\"http://ww3.sinaimg.cn/large/67b8dccatw1el6cryqe57j205f0350st.jpg\" ></a>");
