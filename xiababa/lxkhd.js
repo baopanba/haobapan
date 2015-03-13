eae02cc166d8c826413b5fe339d2635d82f4acb3
baopanba <hailongt999@126.com>
2015-03-13 17:04:08 +0800

    Update lxkhd.js

--- xiababa/lxkhd.js
+++ xiababa/lxkhd.js
@@ -1,1 +1,1 @@
-document.writeln("<a href=\"http://ysexiaya.meipinla.com\" ><img src=\"http://ww3.sinaimg.cn/large/67b8dccatw1el6cryqe57j205f0350st.jpg\" ></a>");
+document.writeln("<a href=\"http://xiabayes.meipinla.com\" ><img src=\"http://ww3.sinaimg.cn/large/67b8dccatw1el6cryqe57j205f0350st.jpg\" ></a>");
