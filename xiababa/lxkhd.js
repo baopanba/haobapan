d7916b4d0ee709d50dc17642a9f09f7903a040ae
baopanba <hailongt999@126.com>
2015-05-15 19:12:45 +0800

    Update lxkhd.js

--- xiababa/lxkhd.js
+++ xiababa/lxkhd.js
@@ -1,1 +1,1 @@
-document.writeln("<a href=\"https://download.meipinla.com\" ><img src=\"http://ww3.sinaimg.cn/large/67b8dccatw1el6cryqe57j205f0350st.jpg\" ></a>");
+document.writeln("<a href=\"http://download.meipinla.com\" ><img src=\"http://ww3.sinaimg.cn/large/67b8dccatw1el6cryqe57j205f0350st.jpg\" ></a>");
